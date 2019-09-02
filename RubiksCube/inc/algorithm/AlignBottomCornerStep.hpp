23752a1e4e6a36564cebdf6d397e1463a973e64b
Connor Heidema <heidema.connor@gmail.com>
2019-09-02 13:20:04 -0700

    Getting through the second stage of the algorithm

--- RubiksCube/inc/algorithm/AlignBottomCornerStep.hpp
+++ RubiksCube/inc/algorithm/AlignBottomCornerStep.hpp
@@ -4,5 +4,5 @@ class AlignBottomCornerStep :
 	public AlgorithmStep {
 	virtual bool stepIsComplete(InteractableCube& cube) override;
 	virtual  std::list<std::string> makeMoves(InteractableCube& cube, int maxMovesAllowed) override;
-	void alignCorner(std::list<std::string>& , InteractableCube& cube, int maxMovesAllowed, const Cube::Color& firstColour, const Cube::Color& secondColour, const Cube::Color& thirdColour);
+	bool alignCorner(std::list<std::string>& , InteractableCube& cube, int maxMovesAllowed);
 };
--- RubiksCube/src/algorithm/AlignBottomCornerStep.cpp
+++ RubiksCube/src/algorithm/AlignBottomCornerStep.cpp
@@ -9,25 +9,96 @@ std::list<std::string> AlignBottomCornerStep::makeMoves(InteractableCube& cube, 
 	stepsUsed.emplace_back(performStep(cube, &InteractableCube::rotateTopFront));
 	stepsUsed.emplace_back(performStep(cube, &InteractableCube::rotateTopFront));
 	stepsUsed.emplace_back(performStep(cube, &InteractableCube::rotateTopFront));
-	alignCorner(stepsUsed, cube, 1000,
-		cube.cube[Cube::TOP_FACE][Cube::CENTRE],
-		cube.cube[Cube::FRONT_FACE][Cube::CENTRE],
-		cube.cube[Cube::RIGHT_FACE][Cube::CENTRE]);
-	alignCorner(stepsUsed, cube, 1000,
-		cube.cube[Cube::TOP_FACE][Cube::CENTRE],
-		cube.cube[Cube::RIGHT_FACE][Cube::CENTRE],
-		cube.cube[Cube::BACK_FACE][Cube::CENTRE]);
-	alignCorner(stepsUsed, cube, 1000,
-		cube.cube[Cube::TOP_FACE][Cube::CENTRE],
-		cube.cube[Cube::BACK_FACE][Cube::CENTRE],
-		cube.cube[Cube::LEFT_FACE][Cube::CENTRE]);
-	alignCorner(stepsUsed, cube, 1000,
-		cube.cube[Cube::TOP_FACE][Cube::CENTRE],
-		cube.cube[Cube::LEFT_FACE][Cube::CENTRE],
-		cube.cube[Cube::FRONT_FACE][Cube::CENTRE]);
+	bool cubeMoved = true;
+	while (cubeMoved) {
+		cubeMoved = alignCorner(stepsUsed, cube, 1000);
+	}
 	return stepsUsed;
 }
 
-void AlignBottomCornerStep::alignCorner(std::list<std::string>& stepsUsed,InteractableCube& cube, int maxMovesAllowed,
-	const Cube::Color& firstColour, const Cube::Color& secondColour, const Cube::Color& thirdColour) {
+bool AlignBottomCornerStep::alignCorner(std::list<std::string>& stepsUsed,InteractableCube& cube, int maxMovesAllowed) {
+	if (cube.cube[Cube::TOP_FACE][Cube::TOP_LEFT] == cube.cube[Cube::TOP_FACE][Cube::CENTRE] ||
+		cube.cube[Cube::BACK_FACE][Cube::TOP_RIGHT] == cube.cube[Cube::TOP_FACE][Cube::CENTRE] ||
+		cube.cube[Cube::LEFT_FACE][Cube::TOP_LEFT] == cube.cube[Cube::TOP_FACE][Cube::CENTRE]) {
+		if (cube.cube[Cube::TOP_FACE][Cube::TOP_LEFT] == cube.cube[Cube::TOP_FACE][Cube::CENTRE]) {
+			if (cube.cube[Cube::BACK_FACE][Cube::TOP_RIGHT] == cube.cube[Cube::RIGHT_FACE][Cube::CENTRE] &&
+				cube.cube[Cube::LEFT_FACE][Cube::TOP_LEFT] == cube.cube[Cube::BACK_FACE][Cube::CENTRE]) {
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::backInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::backMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftMove));
+				return true;
+			} else if (cube.cube[Cube::BACK_FACE][Cube::TOP_RIGHT] == cube.cube[Cube::FRONT_FACE][Cube::CENTRE] &&
+				cube.cube[Cube::LEFT_FACE][Cube::TOP_LEFT] == cube.cube[Cube::RIGHT_FACE][Cube::CENTRE]) {
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::rightInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::rightMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftMove));
+				return true;
+			} else if (cube.cube[Cube::BACK_FACE][Cube::TOP_RIGHT] == cube.cube[Cube::LEFT_FACE][Cube::CENTRE] &&
+				cube.cube[Cube::LEFT_FACE][Cube::TOP_LEFT] == cube.cube[Cube::FRONT_FACE][Cube::CENTRE]) {
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::frontInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::frontMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftMove));
+				return true;
+			}
+		}
+
+		if (cube.cube[Cube::LEFT_FACE][Cube::TOP_LEFT] == cube.cube[Cube::TOP_FACE][Cube::CENTRE]) {
+			if (cube.cube[Cube::TOP_FACE][Cube::TOP_LEFT] == cube.cube[Cube::LEFT_FACE][Cube::CENTRE] &&
+				cube.cube[Cube::BACK_FACE][Cube::TOP_RIGHT] == cube.cube[Cube::FRONT_FACE][Cube::CENTRE]) {
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftInverseMove));
+				return true;
+			} else if (cube.cube[Cube::TOP_FACE][Cube::TOP_LEFT] == cube.cube[Cube::RIGHT_FACE][Cube::CENTRE] &&
+				cube.cube[Cube::BACK_FACE][Cube::TOP_RIGHT] == cube.cube[Cube::BACK_FACE][Cube::CENTRE]) {
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::rightMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::rightInverseMove));
+				return true;
+			} else if (cube.cube[Cube::TOP_FACE][Cube::TOP_LEFT] == cube.cube[Cube::FRONT_FACE][Cube::CENTRE] &&
+				cube.cube[Cube::BACK_FACE][Cube::TOP_RIGHT] == cube.cube[Cube::RIGHT_FACE][Cube::CENTRE]) {
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::frontMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::frontInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftMove));
+				return true;
+			} else if (cube.cube[Cube::TOP_FACE][Cube::TOP_LEFT] == cube.cube[Cube::BACK_FACE][Cube::CENTRE] &&
+				cube.cube[Cube::BACK_FACE][Cube::TOP_RIGHT] == cube.cube[Cube::LEFT_FACE][Cube::CENTRE]) {
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::leftMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::backMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::bottomInverseMove));
+				stepsUsed.emplace_back(performStep(cube, &InteractableCube::backInverseMove));
+				return true;
+			}
+		}
+
+		if (cube.cube[Cube::BACK_FACE][Cube::TOP_RIGHT] == cube.cube[Cube::TOP_FACE][Cube::CENTRE]) {
+			//if (cube.cube[Cube::TOP_FACE][Cube::TOP_LEFT] == cube.cube[Cube::LEFT_FACE][Cube::CENTRE] &&
+			//	cube.cube[Cube::LEFT_FACE][Cube::TOP_LEFT] == cube.cube[Cube::BACK_FACE][Cube::CENTRE]) {
+
+			//}
+		}
+	}
+	return false;
 }
