088e86d2d97f2acde556c9549c187c829fd29dd2
Connor Heidema <heidema.connor@gmail.com>
2019-09-03 10:01:04 -0700

    Added license

--- RubiksCube/src/algorithm/AlignTopCorners.cpp
+++ RubiksCube/src/algorithm/AlignTopCorners.cpp
@@ -20,7 +20,25 @@ bool AlignTopCorners::stepIsComplete(InteractableCube& cube) {
 }
 
 std::list<std::string> AlignTopCorners::makeMoves(InteractableCube& cube, int maxMovesAllowed) {
-	return std::list<std::string>();
+
+	std::list<std::string> stepsUsed;
+	for (int i = 0; i < 4; i++) {
+		if ((cube.cube[Cube::FRONT_FACE][Cube::TOP_RIGHT] == cube.cube[Cube::TOP_FACE][Cube::CENTRE] ||
+			cube.cube[Cube::FRONT_FACE][Cube::TOP_RIGHT] == cube.cube[Cube::FRONT_FACE][Cube::CENTRE] ||
+			cube.cube[Cube::FRONT_FACE][Cube::TOP_RIGHT] == cube.cube[Cube::RIGHT_FACE][Cube::CENTRE]) &&
+			(cube.cube[Cube::TOP_FACE][Cube::BOTTOM_LEFT] == cube.cube[Cube::TOP_FACE][Cube::CENTRE] ||
+			cube.cube[Cube::TOP_FACE][Cube::BOTTOM_LEFT] == cube.cube[Cube::FRONT_FACE][Cube::CENTRE] ||
+			cube.cube[Cube::TOP_FACE][Cube::BOTTOM_LEFT] == cube.cube[Cube::RIGHT_FACE][Cube::CENTRE]) &&
+			(cube.cube[Cube::RIGHT_FACE][Cube::TOP_LEFT] == cube.cube[Cube::TOP_FACE][Cube::CENTRE] ||
+			cube.cube[Cube::RIGHT_FACE][Cube::TOP_LEFT] == cube.cube[Cube::FRONT_FACE][Cube::CENTRE] ||
+			cube.cube[Cube::RIGHT_FACE][Cube::TOP_LEFT] == cube.cube[Cube::RIGHT_FACE][Cube::CENTRE])) {
+			while () {
+				topCornerPermutation(stepsUsed, cube, maxMovesAllowed);
+			}
+		}
+		stepsUsed.emplace_back(performStep(cube, &InteractableCube::rotateRightFront));
+	}
+	return stepsUsed;
 }
 
 void AlignTopCorners::topCornerPermutation(std::list<std::string>& stepsUsed, InteractableCube& cube, int maxMovesAllowed) {
